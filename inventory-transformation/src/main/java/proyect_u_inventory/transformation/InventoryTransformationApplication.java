2543fa7481680946fd15c256f43349df536f32b0
sabastianBaki <jsospina98@hotmail.com>
2025-05-15 20:57:41 -0500

    [FEAT] inicio de proyecto transformacion de inventario

---
+++ inventory-transformation/src/main/java/proyect_u_inventory/transformation/InventoryTransformationApplication.java
@@ -0,0 +1,13 @@
+package proyect_u_inventory.transformation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryTransformationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryTransformationApplication.class, args);
+	}
+
+}
---
+++ inventory-transformation/src/test/java/proyect_u_inventory/transformation/InventoryTransformationApplicationTests.java
@@ -0,0 +1,13 @@
+package proyect_u_inventory.transformation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryTransformationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
